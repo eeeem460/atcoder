385321ef2790d2b54cc81598d0847a2cfbded8bf
desktop <centralrook7@gmail.com>
2022-04-27 22:10:23 +0900

    here

--- atq/atq.cpp
+++ atq/atq.cpp
@@ -1,16 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void read_file(ifstream &input_file, vector<pair<string, string>> &problems) {
-    string tmp_date;
-    string tmp_url;
+struct problem {
+    string index;
+    string name;
+    string date;
+    string url;
 
-    while (input_file >> tmp_date >> tmp_url) {
-        problems.push_back({tmp_date, tmp_url});
+    // edge v;
+    // v.to, v.w でメンバ変数を呼び出す
+    problem(string index, string name, string date, string url) : index(index), name(name), date(date), url(url) {
+    }
+    // graph g(n); で n 頂点のグラフを宣言
+    // graph[from].push_back(edge(to, w)) の形で使う
+};
+
+string get_today_date() {
+    time_t t = time(nullptr);
+    const tm *localTime = localtime(&t);
+
+    std::stringstream s;
+    s << setw(2) << setfill('0') << localTime->tm_year - 100;
+    s << '/';
+    s << setw(2) << setfill('0') << localTime->tm_mon + 1;
+    s << '/';
+    s << setw(2) << setfill('0') << localTime->tm_mday;
+
+    return s.str();
+}
+
+void read_file(ifstream &input_file, vector<pair<string, string>> &problems) {
+    string index;
+    string problem_name;
+    string date;
+    string url;
+
+    while (input_file >> index >> date >> url) {
+        problems.push_back({date, url});
     }
 }
 
-void add_problem(ifstream &input_file, vector<pair<string, string>> &problems) {
+void push_problem(ifstream &input_file, vector<pair<string, string>> &problems) {
     string date;
     string url;
 
@@ -23,10 +53,17 @@ void add_problem(ifstream &input_file, vector<pair<string, string>> &problems) {
 }
 
 void out_console(vector<pair<string, string>> &problems) {
+    string today_date = get_today_date();
+
     for (int i = problems.size() - 1; 0 <= i; i--) {
         auto [date, url] = problems[i];
 
-        cout << i + 1 << ", " << date << ' ' << url << endl << endl;
+        if (date <= today_date) {
+            cout << "\033[31m" << i + 1 << ", " << date << ' ' << url << endl << endl << "\033[m";
+        }
+        else {
+            cout << i + 1 << ", " << date << ' ' << url << endl << endl;
+        }
     }
 }
 
@@ -34,17 +71,32 @@ void out_file(ofstream &output_file, vector<pair<string, string>> &problems) {
     for (int i = 0; i < problems.size(); i++) {
         auto [date, url] = problems[i];
 
-        output_file << date << ' ' << url << "\n\n";
+        string problem_name = "";
+        for (int j = url.size() - 1; j >= 0; j--) {
+            if (url[j] == '/') {
+                break;
+            }
+            else if ('a' <= url[j] and url[j] <= 'z') {
+                problem_name.push_back(toupper(url[j]));
+            }
+            else {
+                problem_name.push_back(url[j]);
+            }
+        }
+
+        reverse(problem_name.begin(), problem_name.end());
+
+        output_file << i + 1 << ", " << problem_name << "  " << date << ' ' << url << "\n\n";
     }
 }
 
-void add() {
+void push() {
     ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
     vector<pair<string, string>> problems;
 
     read_file(input_file, problems);
 
-    add_problem(input_file, problems);
+    push_problem(input_file, problems);
 
     sort(problems.begin(), problems.end());
 
@@ -61,19 +113,53 @@ void look() {
 
     read_file(input_file, problems);
 
+    sort(problems.begin(), problems.end());
+
     out_console(problems);
+
+    ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
+
+    out_file(output_file, problems);
 }
 
-int main(int argc, char *argv[]) {
-    string command = (string)argv[1];
+// void pop(int index) {
+//     ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
+//     vector<pair<string, string>> problems;
 
-    if (command == "add") {
-        add();
+//     read_file(input_file, problems);
+
+//     cout << "Do you erase" << name << "?" << endl;
+
+//     problems.erase(problems.begin() + (index - 1));
+
+//     out_console(problems);
+
+//     ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
+
+//     out_file(output_file, problems);
+// }
+
+int main(int argc, char *argv[]) {
+    ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
+    vector<pair<string, string>> problems;
+
+    read_file(input_file, problems);
+
+    string command = (string)argv[1];
+    if (command == "push") {
+        push();
     }
     else if (command == "look") {
         look();
     }
+    // else if (command == "pop" and argc == 2) {
+    //     pop((int)argv[2]);
+    // }
     else {
         cout << "command eroor" << endl;
     }
+
+    ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
+
+    out_file(output_file, problems);
 }
--- atq/readme.txt
+++ atq/readme.txt
@@ -5,5 +5,5 @@ alias atq="/home/user01/atcoder/atq/atq.out"
 atq look
 キューの中身を見る
 
-atq add 20YYMMDD 問題のURL
+atq push YYMMDD 問題のURL
 問題の追加
