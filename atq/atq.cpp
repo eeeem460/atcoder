6c9404e9e04ef83dccbc2a54e242abba50ea0c31
desktop <centralrook7@gmail.com>
2022-04-27 23:35:00 +0900

    コメント追加

--- atq/atq.cpp
+++ atq/atq.cpp
@@ -14,6 +14,7 @@ bool comp_problem(problem a, problem b) {
     return a.date < b.date;
 }
 
+// 22/04/27 のようなフォーマットで実行時の日付を取得
 string get_today_date() {
     time_t t = time(nullptr);
     const tm *localTime = localtime(&t);
@@ -28,6 +29,8 @@ string get_today_date() {
     return s.str();
 }
 
+// url の末尾から問題名を取得
+// 例えば https://atcoder.jp/contests/abc192/tasks/abc192_e なら ABC192_E
 string get_problem_name(string url) {
     string name = "";
 
@@ -49,6 +52,7 @@ string get_problem_name(string url) {
     return name;
 }
 
+// 入力ファイル queue_1 から problem の配列を作成
 void read_file(ifstream &input_file, vector<problem> &problems) {
     string index;
     string name;
@@ -60,6 +64,7 @@ void read_file(ifstream &input_file, vector<problem> &problems) {
     }
 }
 
+// 配列 problems に標準入力から要素を追加
 void push_problem(vector<problem> &problems) {
     string date;
     string url;
@@ -74,6 +79,7 @@ void push_problem(vector<problem> &problems) {
     problems.push_back(problem(date, name, url));
 }
 
+// pop した瞬間の状態をコンソール出力，pop した行も表示
 void pop_out_console(int index, vector<problem> &problems) {
     for (int i = problems.size() - 1; 0 <= i; i--) {
         if (i == index - 1) {
@@ -86,6 +92,7 @@ void pop_out_console(int index, vector<problem> &problems) {
     }
 }
 
+// 配列 problems をコンソール出力．期限超過分は赤色を着色
 void out_console(vector<problem> &problems) {
     string today_date = get_today_date();
 
@@ -101,6 +108,7 @@ void out_console(vector<problem> &problems) {
     }
 }
 
+// 出力ファイル queue_1 に配列 problems を出力，入力ファイルと出力ファイルは同じことに注意
 void out_file(ofstream &output_file, vector<problem> &problems) {
     for (int i = 0; i < problems.size(); i++) {
         auto [date, name, url] = problems[i];
@@ -109,6 +117,7 @@ void out_file(ofstream &output_file, vector<problem> &problems) {
     }
 }
 
+// 新しい問題を追加
 void push(vector<problem> &problems) {
     push_problem(problems);
 
@@ -117,12 +126,14 @@ void push(vector<problem> &problems) {
     out_console(problems);
 }
 
+// キューの中身を見る
 void look(vector<problem> &problems) {
     sort(problems.begin(), problems.end(), comp_problem);
 
     out_console(problems);
 }
 
+// 問題番号を指定して削除
 void pop(int index, vector<problem> &problems) {
     cout << "Do you erase " << index << ", " << problems[index - 1].name << "?" << endl;
     cout << "please press y or n." << endl;
@@ -144,9 +155,11 @@ void pop(int index, vector<problem> &problems) {
 int main(int argc, char *argv[]) {
     vector<problem> problems;
 
+    // ファイル入力
     ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
     read_file(input_file, problems);
-
+    
+    // コマンドライン引数によって場合分け
     string command = (string)argv[1];
     if (command == "push") {
         push(problems);
@@ -161,6 +174,7 @@ int main(int argc, char *argv[]) {
         cout << "command eroor" << endl;
     }
 
+    // ファイル出力
     ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
     out_file(output_file, problems);
 }
