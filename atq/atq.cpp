1850d18f6ee446edec27195904c669f6eeb65f2d
desktop <centralrook7@gmail.com>
2022-04-27 23:17:28 +0900

    pop

--- atq/atq.cpp
+++ atq/atq.cpp
@@ -2,18 +2,17 @@
 using namespace std;
 
 struct problem {
-    string index;
-    string name;
     string date;
+    string name;
     string url;
 
-    // edge v;
-    // v.to, v.w でメンバ変数を呼び出す
-    problem(string index, string name, string date, string url) : index(index), name(name), date(date), url(url) {
+    problem(string date, string name, string url) : date(date), name(name), url(url) {
     }
-    // graph g(n); で n 頂点のグラフを宣言
-    // graph[from].push_back(edge(to, w)) の形で使う
 };
+
+bool comp_problem(problem a, problem b) {
+    return a.date < b.date;
+}
 
 string get_today_date() {
     time_t t = time(nullptr);
@@ -29,18 +28,39 @@ string get_today_date() {
     return s.str();
 }
 
-void read_file(ifstream &input_file, vector<pair<string, string>> &problems) {
+string get_problem_name(string url) {
+    string name = "";
+
+    for (int j = url.size() - 1; j >= 0; j--) {
+        char c = url[j];
+        if (c == '/') {
+            break;
+        }
+        else if ('a' <= c and c <= 'z') {
+            name.push_back(toupper(c));
+        }
+        else {
+            name.push_back(c);
+        }
+    }
+
+    reverse(name.begin(), name.end());
+
+    return name;
+}
+
+void read_file(ifstream &input_file, vector<problem> &problems) {
     string index;
-    string problem_name;
+    string name;
     string date;
     string url;
 
-    while (input_file >> index >> date >> url) {
-        problems.push_back({date, url});
+    while (input_file >> index >> date >> name >> url) {
+        problems.push_back(problem(date, name, url));
     }
 }
 
-void push_problem(ifstream &input_file, vector<pair<string, string>> &problems) {
+void push_problem(vector<problem> &problems) {
     string date;
     string url;
 
@@ -49,117 +69,98 @@ void push_problem(ifstream &input_file, vector<pair<string, string>> &problems) 
     date.insert(2, "/");
     date.insert(5, "/");
 
-    problems.push_back({date, url});
+    string name = get_problem_name(url);
+
+    problems.push_back(problem(date, name, url));
 }
 
-void out_console(vector<pair<string, string>> &problems) {
+void pop_out_console(int index, vector<problem> &problems) {
+    for (int i = problems.size() - 1; 0 <= i; i--) {
+        if (i == index - 1) {
+            cout << "\033[33m" << "~~~~~~~~~~~~~~~~~~~~~~~~ erased row ~~~~~~~~~~~~~~~~~~~~~~~~" << endl << endl << "\033[m";
+            continue;
+        }
+        auto [date, name, url] = problems[i];
+
+        cout << i + 1 << ", " << date << "  " << name << ' ' << url << endl << endl;
+    }
+}
+
+void out_console(vector<problem> &problems) {
     string today_date = get_today_date();
 
     for (int i = problems.size() - 1; 0 <= i; i--) {
-        auto [date, url] = problems[i];
+        auto [date, name, url] = problems[i];
 
         if (date <= today_date) {
-            cout << "\033[31m" << i + 1 << ", " << date << ' ' << url << endl << endl << "\033[m";
+            cout << "\033[31m" << i + 1 << ", " << date << "  " << name << ' ' << url << endl << endl << "\033[m";
         }
         else {
-            cout << i + 1 << ", " << date << ' ' << url << endl << endl;
+            cout << i + 1 << ", " << date << "  " << name << ' ' << url << endl << endl;
         }
     }
 }
 
-void out_file(ofstream &output_file, vector<pair<string, string>> &problems) {
+void out_file(ofstream &output_file, vector<problem> &problems) {
     for (int i = 0; i < problems.size(); i++) {
-        auto [date, url] = problems[i];
+        auto [date, name, url] = problems[i];
 
-        string problem_name = "";
-        for (int j = url.size() - 1; j >= 0; j--) {
-            if (url[j] == '/') {
-                break;
-            }
-            else if ('a' <= url[j] and url[j] <= 'z') {
-                problem_name.push_back(toupper(url[j]));
-            }
-            else {
-                problem_name.push_back(url[j]);
-            }
-        }
-
-        reverse(problem_name.begin(), problem_name.end());
-
-        output_file << i + 1 << ", " << problem_name << "  " << date << ' ' << url << "\n\n";
+        output_file << i + 1 << ", " << date << "  " << name << ' ' << url << "\n\n";
     }
 }
 
-void push() {
-    ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
-    vector<pair<string, string>> problems;
+void push(vector<problem> &problems) {
+    push_problem(problems);
 
-    read_file(input_file, problems);
-
-    push_problem(input_file, problems);
-
-    sort(problems.begin(), problems.end());
+    sort(problems.begin(), problems.end(), comp_problem);
 
     out_console(problems);
-
-    ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
-
-    out_file(output_file, problems);
 }
 
-void look() {
-    ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
-    vector<pair<string, string>> problems;
-
-    read_file(input_file, problems);
-
-    sort(problems.begin(), problems.end());
+void look(vector<problem> &problems) {
+    sort(problems.begin(), problems.end(), comp_problem);
 
     out_console(problems);
-
-    ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
-
-    out_file(output_file, problems);
 }
 
-// void pop(int index) {
-//     ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
-//     vector<pair<string, string>> problems;
+void pop(int index, vector<problem> &problems) {
+    cout << "Do you erase " << index << ", " << problems[index - 1].name << "?" << endl;
+    cout << "please press y or n." << endl;
 
-//     read_file(input_file, problems);
+    string ans;
+    cin >> ans;
 
-//     cout << "Do you erase" << name << "?" << endl;
-
-//     problems.erase(problems.begin() + (index - 1));
-
-//     out_console(problems);
-
-//     ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
-
-//     out_file(output_file, problems);
-// }
+    if (ans == "n") {
+        return;
+    }
+    else if (ans == "y") {
+        pop_out_console(index, problems);
+        cout << "\n\n\n";
+        problems.erase(problems.begin() + (index - 1));
+        out_console(problems);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
-    vector<pair<string, string>> problems;
+    vector<problem> problems;
 
+    ifstream input_file("/home/user01/atcoder/atq/queue_1.txt");
     read_file(input_file, problems);
 
     string command = (string)argv[1];
     if (command == "push") {
-        push();
+        push(problems);
     }
     else if (command == "look") {
-        look();
+        look(problems);
     }
-    // else if (command == "pop" and argc == 2) {
-    //     pop((int)argv[2]);
-    // }
+    else if (command == "pop" and argc == 3) {
+        pop(stoi((string)argv[2]), problems);
+    }
     else {
         cout << "command eroor" << endl;
     }
 
     ofstream output_file("/home/user01/atcoder/atq/queue_1.txt");
-
     out_file(output_file, problems);
 }
